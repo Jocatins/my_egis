9f3e5a765325011603b3e33239c80a58024ae474
wadetiba <wadetiba@teqbridgeltd.com>
2020-04-02 10:36:25 -0400

    changes on landing to get group of transation per agency

--- src/main/webapp/app/application/landing/landing.component.ts
+++ src/main/webapp/app/application/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { timeout, catchError } from 'rxjs/operators';
 
@@ -6,30 +6,31 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Trans } from '../model/trans.model';
 
-
-
 @Component({
   selector: 'jhi-landing',
   templateUrl: './landing.component.html',
-  styleUrls: [
-    'landing.component.scss'
-  ]
+  styleUrls: ['landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-
   transacations = [
-    {GroupName:"Land Use Allocation Committee", code:"LUAC"},
-    {GroupName:"Directorate of Land Regularisation ", code:"DLR"},
-    {GroupName:"Directorate of Land Services ", code:"DL"},
-    {GroupName:"Directorate of Land Transactions ", code:"DLT"},
-    {GroupName:"Directorate of Land Registry", code:" "},
-    {GroupName:"Survey Unit", code:""},
-    {GroupName:"Commissioner of Stamp Duty ", code:"CoSD"},
-    {GroupName:"New Towns Development Authority ", code:"NTDA"},
-    {GroupName:"Lagos State Public Works Corporation ", code:"LSPWC"}
-  ]
+    { GroupName: 'Land Use Allocation Committee', code: 'LUAC' },
+    { GroupName: 'Directorate of Land Regularisation ', code: 'DLR' },
+    { GroupName: 'Directorate of Land Services ', code: 'DL' },
+    { GroupName: 'Directorate of Land Transactions ', code: 'DLT' },
+    { GroupName: 'Directorate of Land Registry', code: ' ' },
+    { GroupName: 'Survey Unit', code: '' },
+    { GroupName: 'Commissioner of Stamp Duty ', code: 'CoSD' },
+    { GroupName: 'New Towns Development Authority ', code: 'NTDA' },
+    { GroupName: 'Lagos State Public Works Corporation ', code: 'LSPWC' }
+  ];
   message: string;
-  trans: Trans[];
+  transRegistry: Trans[];
+  transLuac: Trans[];
+  transDlr: Trans[];
+  transDls: Trans[];
+  transDlt: Trans[];
+  // Trans[];
+  //  Trans[];
 
   public resourceUrl = 'http://localhost:7777' + '/transinfoWithGroup/';
 
@@ -37,28 +38,92 @@ export class LandingComponent implements OnInit {
     this.message = 'LandingComponent message';
 
     const callResp = this.find('REGISTRY');
-    if ( callResp != null ){
+    if (callResp != null) {
       callResp
-      .pipe( timeout(2000),
-          catchError (e => {
-            alert("Request time out" + e);
+        .pipe(
+          timeout(2000),
+          catchError(e => {
+            alert('Request time out' + e);
             return null;
           })
-      )
-      .subscribe((data : Trans[])=>{
-        this.trans = data;
-      })
-      ;
-    }else{
+        )
+        .subscribe((data: Trans[]) => {
+          this.transRegistry = data;
+        });
+    } else {
       return;
     }
 
+    const callRespLuac = this.find('LUAC');
+    if (callRespLuac != null) {
+      callRespLuac
+        .pipe(
+          timeout(2000),
+          catchError(e => {
+            alert('Request time out' + e);
+            return null;
+          })
+        )
+        .subscribe((data: Trans[]) => {
+          this.transLuac = data;
+        });
+    } else {
+      return;
+    }
+
+    const callRespDlr = this.find('DLR');
+    if (callRespDlr != null) {
+      callRespDlr
+        .pipe(
+          timeout(2000),
+          catchError(e => {
+            alert('Request time out' + e);
+            return null;
+          })
+        )
+        .subscribe((data: Trans[]) => {
+          this.transDlr = data;
+        });
+    } else {
+      return;
+    }
+
+    const callRespDls = this.find('DLS');
+    if (callRespDls != null) {
+      callRespDls
+        .pipe(
+          timeout(2000),
+          catchError(e => {
+            alert('Request time out' + e);
+            return null;
+          })
+        )
+        .subscribe((data: Trans[]) => {
+          this.transDls = data;
+        });
+    } else {
+      return;
+    }
+
+    const callRespDlt = this.find('DLT');
+    if (callRespDlt != null) {
+      callRespDlt
+        .pipe(
+          timeout(2000),
+          catchError(e => {
+            alert('Request time out' + e);
+            return null;
+          })
+        )
+        .subscribe((data: Trans[]) => {
+          this.transDlt = data;
+        });
+    } else {
+      return;
+    }
   }
 
-
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   /*
   private getTransactionInfoByGroup(group: string){
@@ -76,12 +141,8 @@ export class LandingComponent implements OnInit {
   */
 
   find(group: string): Observable<Trans> {
-
     const response = this.httpClient.get<Trans>(`${this.resourceUrl}/${group}`);
 
     return response;
   }
-
-
-
 }
