8748eb77b0c9aedc080b661f042ce141f3ff0483
Samuel Afolabi <safolabi@teqbridgeltd.com>
2020-11-04 09:11:01 +0100

    debugging routes error

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -2,13 +2,6 @@ import { Route } from '@angular/router';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 
 import { HomeComponent } from './home.component';
-import { PROPERTYTAX } from './property-tax-lookup/property-tax-lookup.route';
-import { TAXES } from './property-taxes/property-taxes.route';
-import { ONLINECALCULATOR } from './online-calculator/online-calculator.route';
-import { TRUECOPY } from './certified-true-copy/certified-true-copy.route';
-import { TRACK } from './track-application/track-application.route';
-import { YOURPROPERTY } from './your-property/your-property.route';
-import { USERREG } from './user-registeration/user-registeration.route';
 
 export const HOME_ROUTE: Route = {
   path: '',
@@ -19,5 +12,5 @@ export const HOME_ROUTE: Route = {
     pageTitle: 'home.title'
   },
   canActivate: [UserRouteAccessService],
-  children: [YOURPROPERTY, USERREG, TRACK, TAXES, PROPERTYTAX, TRUECOPY, ONLINECALCULATOR]
+  children: []
 };
