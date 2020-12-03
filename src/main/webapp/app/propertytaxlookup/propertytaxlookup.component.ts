073cad90f555998bf7657f9fe1f49d04c67d4f74
Samuel Afolabi <safolabi@teqbridgeltd.com>
2020-12-03 23:17:18 +0100

    'updates'

--- src/main/webapp/app/propertytaxlookup/propertytaxlookup.component.ts
+++ src/main/webapp/app/propertytaxlookup/propertytaxlookup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { TableFilterPipe } from './table-filter.pipe';
 
 @Component({
   selector: 'jhi-propertytaxlookup',
@@ -7,6 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertytaxlookupComponent implements OnInit {
   message: string;
+
+  instrumentnumber: number;
+  instrunum: number[] = [9328372, 92920283, 938544336, 93735432453, 937382737382];
+
+  titlenumber: number;
+  titlenum: number[] = [44334, 44334, 544336, 5432453, 2737382];
+
+  address: string;
+
+  addre: string[] = ['Oyingbo, Lagos', 'Ikorodu, Lagos', 'Alausa, ikeja Lagos', 'Mile-12, Lagos', 'Maryland, Anthony way, Lagos'];
+
+  users: any[] = [
+    { name: 'Afolabi Samuel', titlenumber: '44334', instrumentnumber: '9328372', address: 'Oyingbo, Lagos' },
+    { name: 'Emmanuel Matthews', titlenumber: '44334', instrumentnumber: '92920283', address: 'Ikorodu, Lagos' },
+    { name: 'Titilayo Racheal', titlenumber: '544336', instrumentnumber: '938544336', address: 'Alausa, ikeja Lagos' },
+    { name: 'Tolulope Ajayi', titlenumber: '5432453', instrumentnumber: '93735432453', address: 'Mile-12, Lagos' },
+    { name: 'Anawana Stephen', titlenumber: '2737382', instrumentnumber: '937382737382', address: 'Maryland, Anthony way, Lagos' }
+  ];
 
   constructor() {
     this.message = 'PropertytaxlookupComponent message';
--- src/main/webapp/app/propertytaxlookup/propertytaxlookup.module.ts
+++ src/main/webapp/app/propertytaxlookup/propertytaxlookup.module.ts
@@ -1,13 +1,16 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { TableFilterPipe } from './table-filter.pipe';
 import { EgisexternalSharedModule } from '../shared/shared.module';
 
 import { PROPERTYTAXLOOKUP_ROUTE, PropertytaxlookupComponent } from './';
 
 @NgModule({
-  imports: [EgisexternalSharedModule, RouterModule.forRoot([PROPERTYTAXLOOKUP_ROUTE], { useHash: true })],
-  declarations: [PropertytaxlookupComponent],
+  imports: [EgisexternalSharedModule, BrowserModule, FormsModule, RouterModule.forRoot([PROPERTYTAXLOOKUP_ROUTE], { useHash: true })],
+  declarations: [PropertytaxlookupComponent, TableFilterPipe],
   entryComponents: [],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
